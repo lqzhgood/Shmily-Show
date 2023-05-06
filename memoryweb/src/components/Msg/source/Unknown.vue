694cf94daa9f897147ccac286b9c06bd8b1fafaa
lqzhgood <9134671+lqzhgood@users.noreply.github.com>
2023-05-06 10:27:26 +0800

    Source Unknown show msg only in isDev

--- memoryweb/src/components/Msg/source/Unknown.vue
+++ memoryweb/src/components/Msg/source/Unknown.vue
@@ -1,25 +1,29 @@
 <template>
-	<MsgWrap>
-		<div class="source-Unknown" v-html="msg.html"></div>
-		{{msg}}
-	</MsgWrap>
+  <MsgWrap>
+    <div class="source-Unknown" v-html="msg.html"></div>
+    <div v-if="isDev">{{ msg }}</div>
+  </MsgWrap>
 </template>
 <script>
-import { CONSOLE_STYLE } from '@/utils/const.js';
+import { CONSOLE_STYLE } from "@/utils/const.js";
 
 export default {
-	name: 'Msg-Source-Unknown',
-	mounted() {
-		CONSOLE_STYLE.danger('未知的消息类型', this.msg);
-	},
-	props: {
-		msg: {
-			type: Object,
-			require: true,
-		},
-	},
-	data: () => ({}),
+  name: "Msg-Source-Unknown",
+  mounted() {
+    CONSOLE_STYLE.danger("未知的消息类型", this.msg);
+  },
+  props: {
+    msg: {
+      type: Object,
+      require: true,
+    },
+  },
+  data: () => ({}),
+  computed: {
+    isDev() {
+      return this.$store.getters["app/isDev"];
+    },
+  },
 };
 </script>
-<style lang="sass" scoped>
-</style>
+<style lang="sass" scoped></style>
