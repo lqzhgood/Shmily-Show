4e526c5842d073f136a1a9f5b4b74c2e6aaefb5f
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-12-16 17:27:51 +0800

    🐞 fix(start.bat): CRLF

--- memoryweb/public/start.bat
+++ memoryweb/public/start.bat
@@ -1,9 +1,14 @@
 @echo off
 
+# 切换到 UTF-8
+chcp 65001
+
+echo 此文件必须为 Windows CRLF 格式
+
 :: 移动到当前目录
 cd %cd%
 
-:: 移动到服务端 
+:: 移动到服务端
 :: 服务端的 web root 配置写的 ..\..\
 cd .\tools\webServer\
 start .\webd.exe
